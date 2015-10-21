6f9adf3b07f53d302d8b163bc28e262296a05ed1
Alex Ramsdell <alexramsdell@gmail.com>
2015-10-21 12:36:44 -0700

    synth and sequencer are working, but need to implement duration

--- src/js/synth_modules/instrument.js
+++ src/js/synth_modules/instrument.js
@@ -81,6 +81,9 @@ var Sequencer = function() {
   this.stopButton = document.getElementById('stop');
   this.playButton = document.getElementById('play');
   this.pad_elements = this.el.querySelectorAll('div');
+  this.tempo = 500;
+  this.bars = 8;
+  this.play_handler;
 
   // model initialization
   this.pads = window.sharedState.pads;
@@ -89,6 +92,23 @@ var Sequencer = function() {
     this.pads[''+i] = new Pad(this.pad_elements[i]);
   }
 
+
+  var self = this;
+  self.playButton.addEventListener('touchstart', function(e) {
+    var i = 0;
+    self.play_handler = setInterval(function() {
+      console.log('beat %s', i);
+      if (window.sharedState.pads[i].sounds.length) {
+        window.sharedState.pads[i].sounds[0].soundSource.start();
+      }
+      i = (i + 1)%8;
+    }.bind(self), 500);
+  });
+
+  this.stopButton.addEventListener('touchstart', function(e) {
+    clearInterval(self.play_handler);
+
+  });
 };
 
 var Pad = function(el) {
