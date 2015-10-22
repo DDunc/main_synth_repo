4ee388649628829c6bd2e1814726cccd64ec3bad
Stan Zhao <zhaostan09@gmail.com>
2015-10-22 11:00:18 -0700

    preset form filter and service

--- src/js/synth-service.js
+++ src/js/synth-service.js
@@ -6,39 +6,16 @@ require("./synth-app.js");
 	angular.module("synthy").service("SynthService", ["$http", function ($http) {
 
 		var SynthCalls = {
-			get: function (id) {
-				if (angular.isDefined(id)) {
-					// return $githubGist(id).read();
-					return $http.get(urlRoot + "/synth/" + id, {
-//						headers: {'Authorization': 'token ' + token,}
-					});
-				} else {
-					// return $http.get(urlRoot);
-					console.warn('root url');
+				getAll: function(){
+					$http.get("api/get_all")
+						.then(function(res){
+							$scope.presets = res.data; 
+						},function(res){
+							console.log(res);
+						});
 				}
-			},
-			update: function (model) {
-				return $http.patch(urlRoot + "/synth/" + model.id, model, {
-//					headers: {
-//						'Authorization': 'token ' + 'value',
-//					}
-				});
-			},
-			create: function (model) {
-				return $http.patch(urlRoot + "/synth/", model, {
-					headers: {
-						'Authorization': 'token ' + token,
-					}
-				});
-			},
-			delete: function (id) {
-				return $http.delete(urlRoot + "/synth/" + id, {
-//					headers: {
-//						'Authorization': 'token ' + 'value',
-//					}
-				});
-			}
 		};
+
 		return SynthCalls;
 	}]);
 
