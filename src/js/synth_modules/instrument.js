569048d8a718b2b6fa4f192167a80a0fbd356eb4
Alex Ramsdell <alexramsdell@gmail.com>
2015-10-22 15:34:42 -0700

    separate sequencer file

--- src/js/synth_modules/instrument.js
+++ src/js/synth_modules/instrument.js
@@ -28,14 +28,9 @@ Key.prototype.touchStart = function (){
     sharedState.keys[keyId].duration = null;
   }.bind(this));
 };
-  // add event handlers
-  //var self = this;
-
-  // touch starts, set note to active, and calc start time
 
 Key.prototype.touchEnd = function () {
     this.el.addEventListener('touchend', function(e){
-    // turn sound off
     this.note.stop();
     var keyId;
     if (e.target.className === 'key') {
@@ -56,14 +51,14 @@ Key.prototype.touchEnd = function () {
 var Instrument = function(ctx) {
 
   // some Dom els
-  // a keys model container
-  // and a reference to shared sharedState object, 'sharedState'
 
   this.el = document.getElementById('instrument');
   this.exportButton = document.getElementById('exportButton');
   this.volumeSlider = document.getElementById('volumeSlider');
   this.keyElements = this.el.querySelectorAll('div');
 
+  // a keys model container
+  // and a reference to shared sharedState object, 'sharedState'
   // model for Instrument - an array of key objects
   this.keys = window.sharedState.keys;
   var note;
@@ -104,45 +99,7 @@ Instrument.prototype.exportInstrumentSettings = function() {
   
 };
 
-var Sequencer = function() {
 
-  // cache dom
-  this.el = document.getElementById('sequencer');
-  this.stopButton = document.getElementById('stop');
-  this.playButton = document.getElementById('play');
-  this.pad_elements = this.el.querySelectorAll('div');
-  this.tempo = 500;
-  this.bars = 8;
-  this.play_handler = null;
-
-  // model initialization
-  this.pads = sharedState.pads;
-  for (var i=0; i < this.pad_elements.length; i++) {
-    // this new pad should have a ref to a dom element
-    this.pads[''+i] = new Pad(this.pad_elements[i]);
-  }
-  // play function on start click
-  var self = this;
-  self.playButton.addEventListener('touchstart', function(e) {
-    var i = 0;
-    self.play_handler = setInterval(function() {
-      console.log('beat %s', i);
-      if (sharedState.pads[i].sounds.length) {
-        console.log(sharedState.pads[i].sounds[0].note);
-        sharedState.pads[i].sounds[0].note.playFor(self.duration);
-        // loop through sounds and start them all
-      }
-      i = (i + 1)%8;
-    }.bind(self), 500);
-  });
-
-  // stop function on stop button click/touch
-  this.stopButton.addEventListener('touchstart', function(e) {
-    clearInterval(self.play_handler);
-  });
-
-
-};
 
 var Pad = function(el) {
 
