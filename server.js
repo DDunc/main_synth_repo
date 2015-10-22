5cad8c551bb2ff104caf49be1037853682463aab
Duncan <Duncan.David@gmail.com>
2015-10-22 10:32:15 -0700

    add get_all to preset_routes, server now using user_routes

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/synth_dev');
 var User = require(__dirname + "/models/user");
 var Preset = require(__dirname + "/models/preset");
 var presetRouter = require(__dirname + "/backend/routes/preset_routes");
+var userRouter = require(__dirname + "/backend/routes/user_routes");
 var ensureAuthenticated = require(__dirname + "/backend/lib/ensureAuth");
-
-
+var findOrCreateUser = require(__dirname + "/backend/lib/find_or_create");
 var FacebookStrategy = require("passport-facebook");
 //var eventEmitter = require("events").EventEmitter;
 //var ee = new EventEmitter();
@@ -34,7 +34,6 @@ var FacebookStrategy = require("passport-facebook");
 var port = process.env.PORT || 3000;
 var GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 var GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-
 var FACEBOOK_APP_ID = process.env.FACEBOOK_APP_ID;
 var FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET;
 
@@ -82,13 +81,6 @@ passport.use(new GoogleStrategy({
   )}));
 
 var app = express();
-app.use('/api', presetRouter);
-
-/* app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-}); */
 
 // configure Express
 app.use(logger('dev'));
@@ -104,118 +96,80 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(__dirname + '/build'));
-
-
-
-app.get('/auth/facebook', passport.authenticate('facebook'),function(req, res){
-  // The request will be redirected to Facebook for authentication, so
-  // this function will not be called.
-});
-
-app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/' }),
-function(req, res) {
-  process.nextTick(function() {
-    findOrCreateUser(req, res, "facebookId");
-  });
-});
-
-  //app.use(express.static(__dirname + '/src/html/login.html'));
-//app.use('/api', usersRouter);
-/* app.get('/', function(req, res){
-  res.send({ user: req.user }); //with no user, sends empty object;
-}); */
-
-app.get('/account', ensureAuthenticated, function(req, res){
-  res.send({ user: req.user });
-});
-
-/* app.get('/login', function(req, res){
-  res.send({ user: req.user });
-}); */
-
-// GET /auth/google
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  The first step in Google authentication will involve
-//   redirecting the user to google.com.  After authorization, Google
-//   will redirect the user back to this application at /auth/google/callback
- app.get('/auth/google',
-  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }),
-  function(req, res){
-    // The request will be redirected to Google for authentication, so this
-    // function will not be called.
-  });
-
-// GET /auth/google/callback
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.
-app.get('/auth/google/return',
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-    console.log("res", res);
-    process.nextTick(function() {
-      findOrCreateUser(req, res, "googleId");
-    });
-  });
-
-//helper function, can be moved to lib
-function findOrCreateUser(req, res, stratId) {
-  User.findOne({strat: req.user.id}, function(err, user) {
-    if (err){
-      return handleError(err, res);
-    }
-    if(user){
-      req.dbId = user._id.toString();
-      console.log(req.dbId);
-      res.body.user = req.user;
-      req.login();
-      res.location("/");
-      res.send({user: user})
-      //res.send(user);
-      //res.redirect("/")
-    }
-    if(!user) {
-      var newUser = new User();
-      req.dbId = newUser._id.toString();
-      console.log("this is new user id " + newUser._id);
-      newUser[stratId] = req.user.id;
-      newUser.displayName = req.user.displayName;
-      console.log(req.dbId);
-
-      var newPreset = new Preset();
-      newPreset.ownerId = req.dbId;
-      newPreset.presetName = req.user.id + " space bass";
-      newPreset.isPublic = true;
-      //newUser.googleProfile = req.user;
-      //function saveDocument(){};
-      //refactor to be
-      newUser.save(function(err, user){
-        if (err){
-          console.log(err);
-        }
-        newPreset.save(function(err, preset){
-          if (err){
-            console.log(err);
-          }
-          //res.send(newUser);
-          res.redirect("/");
-        });
-      });
-    };
-  });
-}
-
-//there i also a req.login request per https://github.com/jaredhanson/passport/blob/master/lib/http/request.js
-app.get('/logout', function(req, res){
-  console.log("logout successful");
-  req.logout();
-  console.log(req.user);
-  res.redirect('/');
-});
+app.use('/api', presetRouter);
+app.use('/auth', userRouter);
 
 app.listen(port, function(){
-  console.log("server is up on port " + port);
+  console.log("I am staying awhile and listening on " + port);
 });
+
+// app.get('/auth/facebook', passport.authenticate('facebook'),function(req, res){
+//   // The request will be redirected to Facebook for authentication, so
+//   // this function will not be called.
+// });
+
+// app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/' }),
+// function(req, res) {
+//   process.nextTick(function() {
+//     findOrCreateUser(req, res, "facebookId");
+//   });
+// });
+
+//   //app.use(express.static(__dirname + '/src/html/login.html'));
+// //app.use('/api', usersRouter);
+// /* app.get('/', function(req, res){
+//   res.send({ user: req.user }); //with no user, sends empty object;
+// }); */
+
+// app.get('/account', ensureAuthenticated, function(req, res){
+//   console.log("account in, authenticated");
+//   res.send({ user: req.user });
+// });
+
+// /* app.get('/login', function(req, res){
+//   res.send({ user: req.user });
+// }); */
+
+// // GET /auth/google
+// //   Use passport.authenticate() as route middleware to authenticate the
+// //   request.  The first step in Google authentication will involve
+// //   redirecting the user to google.com.  After authorization, Google
+// //   will redirect the user back to this application at /auth/google/callback
+//  app.get('/auth/google',
+//   passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }),
+//   function(req, res){
+//     // The request will be redirected to Google for authentication, so this
+//     // function will not be called.
+//   });
+
+// // GET /auth/google/callback
+// //   Use passport.authenticate() as route middleware to authenticate the
+// //   request.  If authentication fails, the user will be redirected back to the
+// //   login page.
+// app.get('/auth/google/return',
+//   passport.authenticate('google', { failureRedirect: '/login' }),
+//   function(req, res) {
+//     console.log("res", res);
+//     process.nextTick(function() {
+//       findOrCreateUser(req, res, "googleId");
+//     });
+//   });
+
+// //helper function, can be moved to lib
+
+// //there i also a req.login request per https://github.com/jaredhanson/passport/blob/master/lib/http/request.js
+// app.get('/logout', function(req, res){
+//   console.log("logout successful");
+//   req.logout();
+//   //console.log(req.user);
+//   res.redirect('/');
+// });
 
 // Simple route middleware to ensure user is authenticated.
 //  Use this function as middleware on any resource that needs to be protected.
+
+/* app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+}); */
